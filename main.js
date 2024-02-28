e0188d1d4a8da63a0980000b093056012d477fbe
Vivian Ho <vivianho711@hotmail.com>
2024-02-28 11:55:57 +0000

    Add format of final output by creating function finalMessage

--- main.js
+++ main.js
@@ -3,3 +3,10 @@ let garmentType = () => {
     let rand = Math.floor(Math.random() * 2);
     return garmentTypeList[rand]
 }
+
+let finalMessage = () => {
+    let garment = garmentType();
+    return 'I would like to knit a ' + garment;
+}
+
+console.log(finalMessage())
